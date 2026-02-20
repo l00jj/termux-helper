a63569c9f18e37e92158440b506bf032b4868aa1
l00 <l00jj@qq.com>
2026-02-20 10:19:05 +0000

    update

--- remote-ssh/remote-ssh-boot.sh
+++ remote-ssh/remote-ssh-boot.sh
@@ -8,8 +8,8 @@ USER_NAME=$(whoami)
 # 获取 IP
 IP_ADDRESS=$(ifconfig 2>/dev/null | grep 'inet ' | grep -v '127.0.0.1' | awk '{print $2}' | head -n 1)
 
-# 可修改最后数字选密码长度
-PASS=$(cat /dev/urandom | tr -dc '0-9' | head -c 4 2>/dev/null)
+# 如需修改长度 "%04d" 与 9999 都要修改
+PASS=$(printf "%04d" $(shuf -i 0-9999 -n 1))
 
 # 设置新密码
 expect -c "spawn passwd; expect \"*password*\"; send \"$PASS\r\"; expect \"*password*\"; send \"$PASS\r\"; expect eof" >/dev/null 2>&1
