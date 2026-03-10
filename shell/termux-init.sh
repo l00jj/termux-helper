aa8fbc7b9db4144afa535ea36df82a2a2837070d
l00 <l00jj@qq.com>
2026-03-10 14:27:22 +0000

    updata

--- shell/termux-init.sh
+++ shell/termux-init.sh
@@ -23,7 +23,7 @@ cat << 'EOF' > "$TEMP_CONFIG"
 # >>> l00 Config Start >>>
 echo -e "\n - - - - - - - - - - - - - - - - - - - - - \n"
 echo "查询进程    : top"
-echo "终止PID进程 : kill <PID>"
+echo "终止PID进程 : kill <PID> (简单退出APP无效)"
 echo "启用 SSH   : bash ~/boot-ssh.sh"
 echo "** 启用 SSH 方法二 : 也可以长按导航栏“菜单”二级“SSH” "
 echo "** 启用 SSH 方法三 : 直接输入 bootssh "
